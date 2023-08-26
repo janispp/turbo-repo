1448fcff4b3b78422017f855d7e362e0ac979954
janis-petronis-visma <janis.petronis@visma.com>
2023-08-26 12:44:59 +0300

    small update

--- apps/product-wrapper/app/page.tsx
+++ apps/product-wrapper/app/page.tsx
@@ -3,7 +3,7 @@ import { Button, Header } from "ui";
 export default function Page() {
   return (
     <>
-      <Header text="Web2" />
+      <Header text="Web s2" />
         <Button/>
     </>
   );
