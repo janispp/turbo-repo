9b9052ffa24486d98bfbb38d9ae9b2b5be595ca3
janis-petronis-visma <janis.petronis@visma.com>
2023-09-04 23:49:30 +0300

    pw2

--- apps/LCD.ProductWrapper/app/page.tsx
+++ apps/LCD.ProductWrapper/app/page.tsx
@@ -3,7 +3,7 @@ import { Button, Header } from "ui";
 export default function Page() {
   return (
     <>
-      <Header text="Product Wrapper" />
+      <Header text="Product Wrapper 22" />
       <Button />
     </>
   );
